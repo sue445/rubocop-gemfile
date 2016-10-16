c38b6c1fa5c16afceae5c008c839c7a6c63b0cc2
sue445 <sue445@sue445.net>
2016-10-16 23:10:04 +0900

    Setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rubocop/gemfile'
+require 'rubocop'
+
+require 'rubocop/rspec/support'
+
+Dir["#{__dir__}/support/**/*.rb"].each(&:require)
+
+RSpec.configure do |config|
+  config.order = :random
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect # Disable `should`
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect # Disable `should_receive` and `stub`
+  end
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rubocop-gemfile'
