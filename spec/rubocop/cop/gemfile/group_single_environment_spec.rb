8c20ef1371a07285d8ebfc415318ee51e29ec576
sue445 <sue445@sue445.net>
2016-10-17 00:13:00 +0900

    Add spec

---
+++ spec/rubocop/cop/gemfile/group_single_environment_spec.rb
@@ -0,0 +1,40 @@
+describe RuboCop::Cop::Gemfile::GroupSingleEnvironment do
+  subject(:cop) { described_class.new }
+
+  context "Single group" do
+    let(:source) do
+      <<-RUBY
+group :test do
+end
+      RUBY
+    end
+
+    it do
+      inspect_source(cop, source)
+
+      aggregate_failures do
+        expect(cop.messages).to be_empty
+        expect(cop.offenses).to be_empty
+      end
+    end
+  end
+
+  context "Muiliple group" do
+    let(:source) do
+      <<-RUBY
+group :test, :development do
+end
+      RUBY
+    end
+
+    it do
+      inspect_source(cop, source)
+
+      aggregate_failures do
+        expect(cop.messages).to eq "`group` should not have multiple environments"
+        expect(cop.offenses.size).to eq 1
+        expect(cop.highlights).to eq([':test, :development'])
+      end
+    end
+  end
+end
