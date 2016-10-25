5ae78f16213f922ed195688bf96b02daf77239e7
sue445 <sue445@sue445.net>
2016-10-26 02:04:51 +0900

    Add TopGems

--- lib/rubocop/cop/gemfile/sort_gem_ascending.rb
+++ lib/rubocop/cop/gemfile/sort_gem_ascending.rb
@@ -6,6 +6,8 @@ module RuboCop
       # This cop makes sure that `gem` is sorted by ascending
       #
       # @example
+      #   # TopGems: []
+      #
       #   # bad
       #   gem "sass-rails"
       #   gem "coffee-rails"
@@ -13,23 +15,46 @@ module RuboCop
       #   # good
       #   gem "coffee-rails"
       #   gem "sass-rails"
+      #
+      #   # TopGems:
+      #   #   - rails
+      #
+      #   # bad
+      #   gem "coffee-rails"
+      #   gem "rails"
+      #   gem "sass-rails"
+      #
+      #   # good
+      #   gem "rails"
+      #   gem "coffee-rails"
+      #   gem "sass-rails"
       class SortGemAscending < Cop
         MSG = "gem should be sorted by ascending".freeze
 
         def on_begin(node)
           gems = string_arg_gems(node)
+
           gems.each_cons(2) do |gem1, gem2|
             gem_name1 = gem_name(gem1)
             gem_name2 = gem_name(gem2)
 
-            add_offense(node, gem2.loc.expression) if gem_name1 > gem_name2
+            if !top_gem?(gem1) && top_gem?(gem2)
+              add_offense(node, gem2.loc.expression, "gem '#{gem_name2}' should be top of Gemfile")
+            elsif top_gem?(gem1) && !top_gem?(gem2)
+              # nop
+            else
+              add_offense(node, gem2.loc.expression) if gem_name1 > gem_name2
+            end
           end
         end
 
         def autocorrect(node)
           lambda do |corrector|
             gems        = string_arg_gems(node)
-            sorted_gems = gems.sort_by { |gem| gem_name(gem) }
+            sorted_gems = gems.sort_by do |gem|
+              sort_key = top_gem?(gem) ? 0 : 1
+              [sort_key, gem_name(gem)]
+            end
 
             gems_enum        = gems.to_enum
             sorted_gems_enum = sorted_gems.to_enum
@@ -67,6 +92,21 @@ module RuboCop
           return nil unless arg.str_type?
 
           arg.str_content
+        end
+
+        def top_gems
+          @top_gems ||= Array(cop_config["TopGems"])
+        end
+
+        def top_gem?(gem)
+          gem_name =
+            if gem.is_a?(RuboCop::Node)
+              gem_name(gem)
+            else
+              gem
+            end
+
+          top_gems.include?(gem_name)
         end
       end
     end
--- spec/rubocop/cop/gemfile/sort_gem_ascending_spec.rb
+++ spec/rubocop/cop/gemfile/sort_gem_ascending_spec.rb
@@ -1,5 +1,12 @@
-describe RuboCop::Cop::Gemfile::SortGemAscending do
-  subject(:cop) { described_class.new }
+describe RuboCop::Cop::Gemfile::SortGemAscending, :config  do
+  subject(:cop) { described_class.new(config) }
+
+  let(:cop_config) {
+    {
+      "TopGems" => top_gems,
+    }
+  }
+  let(:top_gems) { [] }
 
   context "With ascending 1" do
     let(:source) do
@@ -100,6 +107,51 @@ gem 'uglifier', '>= 1.3.0'
         expect(cop.messages).to eq ["gem should be sorted by ascending", "gem should be sorted by ascending"]
         expect(cop.offenses.size).to eq 2
         expect(cop.highlights).to eq([%q(gem 'pg', '~> 0.18'), %q(gem 'coffee-rails', '~> 4.2')])
+      end
+    end
+
+    it "auto-correct" do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(expected_source)
+    end
+  end
+
+  context "Without ascending 3" do
+    let(:top_gems) { %w(rails) }
+
+    let(:source) do
+      <<-RUBY
+source "https://rubygems.org"
+
+gem 'pg', '~> 0.18'
+gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'puma', '~> 3.0'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+      RUBY
+    end
+
+    let(:expected_source) do
+      <<-RUBY
+source "https://rubygems.org"
+
+gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'coffee-rails', '~> 4.2'
+gem 'pg', '~> 0.18'
+gem 'puma', '~> 3.0'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+      RUBY
+    end
+
+    it do
+      inspect_source(cop, source)
+
+      aggregate_failures do
+        expect(cop.messages).to eq ["gem 'rails' should be top of Gemfile", "gem should be sorted by ascending"]
+        expect(cop.offenses.size).to eq 2
+        expect(cop.highlights).to eq([%q(gem 'rails', '~> 5.0.0', '>= 5.0.0.1'), %q(gem 'coffee-rails', '~> 4.2')])
       end
     end
 
