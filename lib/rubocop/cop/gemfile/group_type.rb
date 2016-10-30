79d786ba503facedfeb3ee151a9b109c99ac0c98
sue445 <sue445@sue445.net>
2016-10-31 00:29:31 +0900

    Refactor: Gemfile::GroupType

--- lib/rubocop/cop/gemfile/group_type.rb
+++ lib/rubocop/cop/gemfile/group_type.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'rubocop/cop/gemfile/concerns/group_type/on_send'
+require 'rubocop/cop/gemfile/concerns/group_type/autocorrect'
+
 module RuboCop
   module Cop
     module Gemfile
@@ -60,27 +63,18 @@ module RuboCop
         ENFORCED_STYLE_DOUBLE_QUOTES = 'double_quotes'.freeze
         ENFORCED_STYLE_SINGLE_QUOTES = 'single_quotes'.freeze
 
+        VALIDATION_ENFORCED_STYLE_MSG = '%s is not unsupport'.freeze
+
+        include Concerns::GroupType::OnSend
+        include Concerns::GroupType::Autocorrect
+
         def on_send(node)
           _, method_name, *args = *node
 
           return unless method_name == :group
 
-          enforced_style = cop_config['EnforcedStyle']
-          unless cop_config['SupportedStyles'].include?(enforced_style)
-            raise ValidationError, "EnforcedStyle(#{enforced_style}) is not neither double_quotes, single_quotes or symbol"
-          end
-
-          message = MSG % enforced_style
-
           args.each do |arg|
-            case enforced_style
-            when ENFORCED_STYLE_SYMBOL
-              add_offense(node, arg.loc.expression, message) if arg.str_type?
-            when ENFORCED_STYLE_DOUBLE_QUOTES
-              add_offense(node, arg.loc.expression, message) if arg.sym_type? || single_quotes?(arg)
-            when ENFORCED_STYLE_SINGLE_QUOTES
-              add_offense(node, arg.loc.expression, message) if arg.sym_type? || double_quotes?(arg)
-            end
+            check_group_argument(arg, node)
           end
         end
 
@@ -89,30 +83,41 @@ module RuboCop
 
           case cop_config['EnforcedStyle']
           when ENFORCED_STYLE_SYMBOL
-            lambda do |corrector|
-              args.each do |arg|
-                content = arg_content(arg)
-                corrector.replace(arg_location(arg), to_symbol_literal(content)) if content
-              end
-            end
+            correct_symbol_group(args)
           when ENFORCED_STYLE_DOUBLE_QUOTES
-            lambda do |corrector|
-              args.each do |arg|
-                content = arg_content(arg)
-                corrector.replace(arg_location(arg), content.inspect) if content
-              end
-            end
+            correct_double_quote_group(args)
           when ENFORCED_STYLE_SINGLE_QUOTES
-            lambda do |corrector|
-              args.each do |arg|
-                content = arg_content(arg)
-                corrector.replace(arg_location(arg), to_string_literal(content)) if content
-              end
-            end
+            correct_symbol_quote_group(args)
           end
         end
 
         private
+
+        def check_group_argument(arg, node)
+          message = MSG % enforced_style
+
+          case enforced_style
+          when ENFORCED_STYLE_SYMBOL
+            check_symbol_group(arg, node, message)
+          when ENFORCED_STYLE_DOUBLE_QUOTES
+            check_double_quotes_group(arg, node, message)
+          when ENFORCED_STYLE_SINGLE_QUOTES
+            check_single_quotes_group(arg, node, message)
+          end
+        end
+
+        def enforced_style
+          return @enforced_style if @enforced_style
+
+          @enforced_style = cop_config['EnforcedStyle']
+
+          unless cop_config['SupportedStyles'].include?(enforced_style)
+            message = VALIDATION_ENFORCED_STYLE_MSG % enforced_style
+            raise ValidationError, message
+          end
+
+          @enforced_style
+        end
 
         def single_quotes?(arg)
           quotes?(arg, "'")
@@ -123,13 +128,23 @@ module RuboCop
         end
 
         def quotes?(arg, quote)
-          arg.str_type? && arg.loc.expression.source_buffer.source[arg.loc.begin.begin_pos] == quote
+          return false unless arg.str_type?
+
+          arg_prev_position = arg.loc.begin.begin_pos
+          arg.loc.expression.source_buffer.source[arg_prev_position] == quote
+        end
+
+        def exclude_type?(arg)
+          return false if arg.str_type? || arg.sym_type?
+          true
         end
 
         def arg_location(arg)
           begin_pos = arg.loc.begin.begin_pos
           end_pos = arg.loc.expression.end_pos
-          Parser::Source::Range.new(arg.loc.expression.source_buffer, begin_pos, end_pos)
+
+          Parser::Source::Range.new(arg.loc.expression.source_buffer,
+                                    begin_pos, end_pos)
         end
 
         def arg_content(arg)
